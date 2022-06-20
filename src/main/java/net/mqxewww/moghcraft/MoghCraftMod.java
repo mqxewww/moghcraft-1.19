04ecca6f876fb0966da22a255d989d28e3944a9f
25thMaxouuu <noizetmaxencepro@gmail.com>
2022-06-20 19:02:46 +0200

    Initial commit

---
+++ src/main/java/net/mqxewww/moghcraft/MoghCraftMod.java
@@ -0,0 +1,13 @@
+package net.mqxewww.moghcraft;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoghCraftMod implements ModInitializer {
+	public static final String MOD_ID = "moghcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/net/mqxewww/moghcraft/MoghCraftModClient.java
@@ -0,0 +1,8 @@
+package net.mqxewww.moghcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoghCraftModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
